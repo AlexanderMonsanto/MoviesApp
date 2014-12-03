93a1f31e30a1a22b0dcb8d5dddeaef3d03a83b29
alexandermonsanto <alexandermonsanto@gmail.com>
2014-12-02 22:01:20 -0800

    dynamic movie app

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+var searchterm = "Twighlight";
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(__dirname + "/public"))
+
+app.set("view engine", 'ejs');
+
+app.get('/',function(req, res){
+  res.render('moviesite/index')
+
+})
+
+app.get('/search',function(req, res){
+  var request = require('request');
+
+request('http://www.omdbapi.com/?s=' + req.query.pelicula, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    // res.send(info);
+    res.render("moviesite/search", info);
+  }
+})
+})
+
+app.get('/moviesite/:imdbID', function(req, res){
+  var ID = req.params.imdbID
+  request('http://www.omdbapi.com/?i=' + ID, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info);
+    res.render("moviesite/imdb", info);
+  } else { alert:"Not a Movie"}
+})
+})
+
+app.listen(3000);
